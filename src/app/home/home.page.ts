c91f24a1c21031f2b2f62ef6095de82918f57058
Brendis-max <20233l001111@utcv.edu.mx>
2025-01-20 22:28:40 -0600

    Validaciones login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,7 +14,8 @@ export class HomePage {
 
   constructor(private navController: NavController, private toastController: ToastController) {}
 
-
+  // validaciones de login
+  
   validateEmail(email: string): boolean {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
